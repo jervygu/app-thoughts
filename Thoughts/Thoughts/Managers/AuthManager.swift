b541b4fe2bdba787a0ea1434b9f82fffb8f553aa
jervygu22 <jgumandap@codedisruptors.com>
2022-07-26 11:52:46 +0800

    AuthManager setup

--- Thoughts/Thoughts/Managers/AuthManager.swift
+++ Thoughts/Thoughts/Managers/AuthManager.swift
@@ -11,8 +11,59 @@ import FirebaseAuth
 final class AuthManager {
     static let shared = AuthManager()
     
-    private let database = Auth.auth()
+    private let auth = Auth.auth()
     
     private init() {}
+    
+    public var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    public func signUp(email: String, password: String, completion: @escaping(Bool) -> Void) {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6 else {
+            return
+        }
+        
+        auth.createUser(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                completion(false)
+                return
+            }
+            // account created
+            completion(true)
+        }
+    }
+    
+    
+    public func signIn(email: String, password: String, completion: @escaping(Bool) -> Void) {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6 else {
+            return
+        }
+        
+        auth.signIn(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                completion(false)
+                return
+            }
+            
+            // account signed in
+            completion(true)
+        }
+    }
+    
+    public func signOut(completion: (Bool) -> Void) {
+        do {
+            try auth.signOut()
+            completion(true)
+        } catch {
+            print("signOut error: \(error.localizedDescription)")
+            completion(false)
+        }
+        
+    }
     
 }
