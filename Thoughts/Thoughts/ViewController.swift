853d46d1ff294703d9d904f874371d1056e4dcc4
jervygu22 <jgumandap@codedisruptors.com>
2022-07-26 09:27:55 +0800

    starter project

---
+++ Thoughts/Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by CDI on 7/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
